ccd5608210da577bd142c0f08dfe6a272135d6c5
Corsa21 <kirill.assassin@yandex.ru>
2022-01-10 02:18:22 +0700

    частичная реализация

---
+++ script.js
@@ -0,0 +1,97 @@
+let a = ''; // first number
+let b = ''; // second number
+let sign = ''; // symbol
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['+', '-', '/', 'X', '%', '+/-'];
+
+// экран вывода
+const out = document.querySelector('.calc-screen p')
+
+function clearAll() {
+    a = ''; // first number
+    b = ''; // second number
+    sign = ''; // symbol
+    finish = false;
+    out.textContent = '';
+}
+
+document.querySelector('.ac').addEventListener('click', clearAll);
+
+document.querySelector('.buttons').addEventListener('click', (event) => {
+    //нажата не кнопка
+    if (!event.target.classList.contains('btn')) return;
+    //нажата кнопка clearAll ac
+    if (event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+
+    //получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    //если нажата кнопка (0-9,.)
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+            out.textContent = a;
+        } else if (a !== '' && b !== '' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        } else {
+            b += key;
+            out.textContent = b;
+        }
+    }
+
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+    }
+
+    //если нажата кнопка '='
+    if (key === '=') {
+        if (b === '') {
+            a *= a;
+
+        }
+        if (sign.includes('X')) {
+            a = Number(a) * Number(b);
+        } else if (sign.includes('/')) {
+            if (b === '0') {
+                out.textContent = 'Ошибка';
+                a = '';
+                b = '';
+                sign = '';
+                return
+            }
+            a = Number(a) / Number(b);
+        } else if (sign.includes('-')) {
+            a = Number(a) - Number(b);
+        } else if (sign.includes('%')) {
+            a = Number(a) % Number(b);
+        } else if (sign.includes('+')) {
+            a = Number(a) + Number(b);
+        }
+        finish = true;
+        out.textContent = a;
+    }
+
+    //если нажата кнопка '+/-'
+    if (sign.includes('+/-')) {
+        if (b !== '' && a !== '') {
+        } else {
+            if (b === '') {
+                a = -a
+                out.textContent = a;
+            } else {
+                b = -b
+                out.textContent = b;
+            }
+        }
+    }
+
+
+    console.log(a, b, sign)
+});
